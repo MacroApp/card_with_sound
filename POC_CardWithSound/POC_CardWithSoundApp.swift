2ad5d3bd89d8986d739f1bf5370267ad8e60ca09
AIWASS23 <marcelodearaujo677@gmail.com>
2023-09-05 08:27:37 -0300

    Initial Commit

---
+++ POC_CardWithSound/POC_CardWithSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_CardWithSoundApp.swift
+//  POC_CardWithSound
+//
+//  Created by Marcelo De Araújo on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct POC_CardWithSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
